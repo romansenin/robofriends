df0d23c652e3d0eb174854cbc78cf7d2d46563b4
Roman Senin <romansenin15@gmail.com>
2023-02-20 18:00:11 -0800

    feat: filter robots by search

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
   return (
     <>
       <input
@@ -8,6 +8,7 @@ const SearchBox = () => {
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
+        onChange={searchChange}
       />
     </>
   );
