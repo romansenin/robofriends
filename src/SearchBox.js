5a1f17996c1edf16694704d1ff59331aade070b3
Roman Senin <romansenin15@gmail.com>
2023-02-20 17:14:39 -0800

    feat: create SearchBox

---
+++ src/SearchBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBox = () => {
+  return (
+    <>
+      <input
+        id="searchBox"
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+      />
+    </>
+  );
+};
+
+export default SearchBox;
