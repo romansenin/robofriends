4bcbadeaa06b8bac7259857a3161cbd307ecfcd1
Roman Senin <romansenin15@gmail.com>
2023-02-21 21:42:47 -0800

    feat: add styles to app

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Card = ({ id, name, email }) => {
   return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow">
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow card-container">
       <img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
-      <div>
+      <div className="card-details">
         <h2>{name}</h2>
         <p>{email}</p>
       </div>
