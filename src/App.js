d05f09ce6a6bbf57cc8aecd7e123494f7aa5b266
Roman Senin <romansenin15@gmail.com>
2023-03-03 16:48:16 -0800

    feat: fetch user data from json placeholder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange(event) {
@@ -22,6 +27,9 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter((robot) =>
       robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     );
+    if (this.state.robots.length === 0) {
+      return <h1>Loeading</h1>;
+    }
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
